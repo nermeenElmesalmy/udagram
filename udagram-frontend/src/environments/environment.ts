a7d5572b498e79c6033fd6163f08a3722f07af9e
nermeenElmesalmy <101740899+nermeenElmesalmy@users.noreply.github.com>
2022-05-18 00:06:50 +0200

    update 4

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://udagram-api-dev.eba-bnpajavd.us-east-1.elasticbeanstalk.com'
+  apiHost: 'http://udagram-api-dev.eba-bnpajavd.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
